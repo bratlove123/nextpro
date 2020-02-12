74083cc7673b81a8371bee76359c7936a3d062ef
hothaiphong <thaiphong.spkt@gmail.com>
2020-02-12 09:07:43 +0700

    Added solution

---
+++ components/layouts/header/header.tsx
@@ -0,0 +1,7 @@
+import './header.scss';
+
+function Header() {
+  return <div className="header">Header hihi!</div>;
+}
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import Header from '@components/layouts/header/header';
+
+function HomePage() {
+  return <div><Header/>Welcome to Next.js!</div>;
+}
+
+export default HomePage;
